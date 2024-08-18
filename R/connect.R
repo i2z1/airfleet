4cef4a3e8c4afd7fa1c775523e0eb81a8db99bef
i2z1 <i2z1@yandex.ru>
2024-08-19 00:31:22 +0300

    init

---
+++ R/connect.R
@@ -0,0 +1,50 @@
+#' Make connection object
+#'
+#' @param host Fleet host
+#' @param port Fleet port (default 443)
+#' @param apitoken Fleet instance API token
+#'
+#' @return con object
+#' @export
+flt_connection <- function(host = "127.0.0.1",
+                            port = 443,
+                            apitoken){
+  list(host = host,
+       port = port,
+       token = apitoken)
+}
+
+#' Generate request to API endpoint
+#'
+#' @param endpoint string, API path with params
+#' @param flt_con connection object via flt_connection()
+#' @param ...
+#'
+#' @return list with deJSONed object
+#' @export
+#' @import httr2
+make_flt_req <- function(endpoint, flt_con, ...){
+  flt_url <- paste0("https://", flt_con$host, ":", flt_con$port)
+  endpoint_params <- paste0(endpoint, ...)
+
+  ans <- httr2::request(flt_url) |>
+    httr2::req_url_path_append(endpoint_params) |>
+    httr2::req_auth_bearer_token(flt_con$token) |>
+    httr2::req_perform()
+
+  ans_list <- ans |>
+    httr2::resp_body_json()
+
+  return(ans_list)
+
+}
+
+#' Get hosts
+#'
+#' @param flt_con connection object
+#'
+#' @return df
+#' @export
+get_hosts <- function(flt_con){
+  res <- make_flt_req("/api/v1/fleet/hosts", flt_con)
+}
